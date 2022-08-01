4531b2da41082bcb022a92bb57dfca8429211e99
Andrey Kolkov <androndo@gmail.com>
2022-08-01 13:13:13 +0400

    fix bootstrap overwrites

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,6 +9,17 @@ fi
 
 ln -sf "${HOME}/dotfiles/shell.sh" "${HOME}/.shell.sh"
 
-add_source_cmd='\n# Source our shell configuration if it exists.\n[ -r ~/.shell.sh ] && source ~/.shell.sh\n'
-echo -e ${add_source_cmd} >> ${HOME}/.bashrc
-echo -e ${add_source_cmd} >> ${HOME}/.zshrc
+
+# Source our shell configuration in any local shell config files.
+config_files=(~/.bashrc ~/.zshrc)
+for config_file in ${config_files[@]}; do
+    # Skip config files that don't exist.
+    [ -r "${config_file}" ] || continue
+
+    # If we don't have the 'source ~/.shell.d' line in our config, add it.
+    source_command="[ -r ~/.shell.sh ] && source ~/.shell.sh"
+    if ! grep -q "${source_command}" "${config_file}"; then
+        echo ".shell.sh is not sourced in '${config_file}' adding this now..."
+        echo "${source_command}" >> "${config_file}"
+    fi
+done
